0593e401929280a7dd1a308cc78dcf092f30ea26
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-07-18 17:42:16 -0500

    ✨ page calendar in progress

--- src/app/(routes)/tasks/components/Calendar/Calendar.tsx
+++ src/app/(routes)/tasks/components/Calendar/Calendar.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import multiMonth from '@fullcalendar/multimonth';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import { CalendarProps } from './Calendar.types';
 
 export const Calendar = (props: CalendarProps) => {
