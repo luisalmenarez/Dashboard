84a012af24b82f05b6337ef9a62313f28c60b1b0
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-07-22 16:21:37 -0500

    ✨ Add page calendar and funcionts

---
+++ src/lib/formatDate.ts
@@ -0,0 +1,22 @@
+export const formatDate = (date: Date) => {
+  const months = [
+    'ene',
+    'feb',
+    'mar',
+    'abr',
+    'may',
+    'jun',
+    'jul',
+    'ago',
+    'sep',
+    'oct',
+    'nov',
+    'dic',
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+};
